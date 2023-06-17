1da401951ec21e4a988b688407807a87f6764eee
hmz1998 <mohammadiyan.hassan1377@gmail.com>
2023-06-17 22:00:59 +0330

    reformated codes with black

--- posts/common.py
+++ posts/common.py
@@ -3,14 +3,8 @@ from django.core.validators import MaxLengthValidator, MinLengthValidator
 
 
 class TimeStampModelMixin(models.Model):
-    created_at = models.DateTimeField(
-        verbose_name="Created At",
-        auto_now_add=True
-    )
-    modified_at = models.DateTimeField(
-        verbose_name="Modified At",
-        auto_now=True
-    )
+    created_at = models.DateTimeField(verbose_name="Created At", auto_now_add=True)
+    modified_at = models.DateTimeField(verbose_name="Modified At", auto_now=True)
 
     class Meta:
         abstract = True
@@ -20,18 +14,15 @@ class TitleSlugLinkModelMixin(models.Model):
     title = models.CharField(
         verbose_name="Title",
         max_length=150,
-        validators=[
-            MaxLengthValidator(150),
-            MinLengthValidator(3)
-        ],
-        unique=True
+        validators=[MaxLengthValidator(150), MinLengthValidator(3)],
+        unique=True,
     )
     slug = models.SlugField(
         verbose_name="Slug",
         editable=False,
         allow_unicode=True,
         max_length=150,
-        unique=True
+        unique=True,
     )
 
     class Meta:
