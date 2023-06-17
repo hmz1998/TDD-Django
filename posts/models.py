c47b596e93d9f43bfe5bfb251918e8a524fefae0
hmz1998 <mohammadiyan.hassan1377@gmail.com>
2023-06-17 22:02:51 +0330

    fixed the post model bugs and added get absolute url method in this model.

--- posts/models.py
+++ posts/models.py
@@ -1,17 +1,31 @@
 from django.db import models
+from django.urls import reverse
+from django.utils.text import slugify
+
 from .common import (
     TimeStampModelMixin,
     TitleSlugLinkModelMixin,
 )
+
 
 class Post(TimeStampModelMixin, TitleSlugLinkModelMixin):
     description = models.TextField(
         verbose_name="Description",
         max_length=1000,
     )
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title, allow_unicode=True)
+        return super(Post, self).save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return self.title
     
+    def get_absolute_url(self):
+        return reverse('post_detail', kwargs={"slug": self.slug})
+    
+
     class Meta:
         verbose_name = "Post"
         verbose_name_plural = "Posts"
-        ordering = ('-created_at', )
-    
+        ordering = ("-created_at",)
