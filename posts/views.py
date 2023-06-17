ae99bae444d13714b5ef25e07a4c90b25ee89043
hmz1998 <mohammadiyan.hassan1377@gmail.com>
2023-06-17 22:05:17 +0330

    added home page view and post detail page view.

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Post
+
+def index(request):
+    posts = Post.objects.all()
+    return render(request, "posts/index.html", {"posts": posts})
+
+
+def post_detail(request, slug):
+    return render(request, "posts/detail.html")
