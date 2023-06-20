3ef786f47b3f03065b16f50d77bcd66691eb0d7d
hmz1998 <mohammadiyan.hassan1377@gmail.com>
2023-06-20 21:13:26 +0330

    login page test was created.

---
+++ accounts/test/test_login.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from django.urls import reverse
+from http import HTTPStatus
+
+
+class LoginTest(TestCase):
+    def setUp(self) -> None:
+        pass
+
+    def test_login_page_exists(self):
+        response = self.client.get(reverse('accounts:login_page'))
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, 'accounts/login.html')
