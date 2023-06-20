ae007aa1ebc1bd2e3785fc78cf153392898a6475
hmz1998 <mohammadiyan.hassan1377@gmail.com>
2023-06-20 19:49:19 +0330

    created signup page view.

---
+++ accounts/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .forms import UserRegistrationForm
+
+
+def register_user(request):
+    form = UserRegistrationForm()
+
+    context = {'form': form}
+    return render(request, 'accounts/register.html', context)
