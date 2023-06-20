d14c0c02278b46720c59db402f91cc044d949102
hmz1998 <mohammadiyan.hassan1377@gmail.com>
2023-06-20 21:15:23 +0330

    login page view function was created. and url path was fixed.

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 app_name = 'accounts'
 urlpatterns = [
-    path('signup/', views.register_user, name='signup_page')
+    path('signup/', views.register_user, name='signup_page'),
+    path('login/', views.login_page, name='login_page')
 ]
